e3f14d749a204cf770655187b72b6097a7411eef
Jack-Kilrain <toastcraft.info@gmail.com>
2021-03-31 12:41:53 +1100

    Updated GitHub workflow

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
-curl -X POST \
+curl -v -X POST \
       -H 'Circle-Token: a7ba232028b86cff5396141a7dfa7f25bf451eba' \
-      -H 'Content-Type: application/json' \
-      -H 'Accept: application/json' \
       -d "{\"branch\":\"INS-12888-Enable-CircleCI-Build-Test-On-Commit\", \"parameters\":{\"is-pr\":true}}" \
-      https://circleci.com/api/v2/project/github/EngineersBox/terraform-provider-instaclustr/pipeline
+      https://circleci.com/api/v2/project/gh/EngineersBox/terraform-provider-instaclustr/pipeline
