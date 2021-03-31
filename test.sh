9c709009a754242092469c911eb22fc446033af6
Jack-Kilrain <toastcraft.info@gmail.com>
2021-03-31 12:33:27 +1100

    Updated CircleCI config naming and division of events

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-if [ false ]; then
-    echo "test"
-else
-  echo "other"
-fi
+curl -X POST \
+      -H 'Circle-Token: a7ba232028b86cff5396141a7dfa7f25bf451eba' \
+      -H 'Content-Type: application/json' \
+      -H 'Accept: application/json' \
+      -d "{\"branch\":\"INS-12888-Enable-CircleCI-Build-Test-On-Commit\", \"parameters\":{\"is-pr\":true}}" \
+      https://circleci.com/api/v2/project/github/EngineersBox/terraform-provider-instaclustr/pipeline
